9a65bfefd239febf7bbcb25cb50367c06530afdb
jlearna <jlearna133@gmail.com>
2020-02-01 16:27:28 +0100

    initial commit

---
+++ src/main/java/com/learn/oc/LearnOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.learn.oc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOpenshiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnOpenshiftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/oc/LearnOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.oc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnOpenshiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
