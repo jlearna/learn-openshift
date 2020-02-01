dcd7e231955463d78008f4d3cc8b8631daff76d9
jlearna <jlearna133@gmail.com>
2020-02-01 16:32:35 +0100

    change made

--- src/main/java/com/learn/oc/LearnOpenshiftApplication.java
+++ src/main/java/com/learn/oc/LearnOpenshiftApplication.java
@@ -2,9 +2,15 @@ package com.learn.oc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LearnOpenshiftApplication {
+
+    public String welcome(){
+        return "Hi Open Shift Works!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LearnOpenshiftApplication.class, args);
